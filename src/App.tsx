b3b2dd4bd45fe61cd8375baec9b5523b82492e4e
Irakli Kverenchkhiladze <irakli_kverenchkhiladze@epam.com>
2023-02-01 20:35:17 +0400

    test

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from 'react';
+import { animate, stopAnimation, rendererDomElement } from './scene';
+
+/**
+ * Entry point of the app
+ *
+ * @returns {JSX.Element} JSX Element
+ */
+function App(): JSX.Element {
+  const sceneRef = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (sceneRef.current) {
+      console.log("started");
+      sceneRef.current.append(rendererDomElement);
+      animate();
+    }
+
+    return () => {
+      sceneRef.current?.removeChild(rendererDomElement);
+      stopAnimation();
+      console.log("stopped");
+    }
+  }, [])
+
+  return <div className='bg-black' ref={sceneRef}>
+    <h1 className='font-bold text-white text-md'>Hello</h1>
+  </div>
+}
+
+export default App
